36fb518d9b5aed80265eef1d395a37fa2d861162
ProkiProk <senior.prockuschev2017@yandex.ru>
2021-11-29 09:47:15 +0300

    v0.1

--- sources/sharedprt.cpp
+++ sources/sharedprt.cpp
@@ -1,8 +1,8 @@
-// Copyright 2021 Your Name <your_email>
+//
+// Created by prokiprok on 29.11.2021.
+//
 
 #include <stdexcept>
-
-#include <example.hpp>
 
 auto example() -> void {
   throw std::runtime_error("not implemented");
